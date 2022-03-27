68b612a6f4360ea24a8f572fbb7033e4ad83306c
Sora Arakawa <mail@arkw.net>
2022-03-27 16:36:01 +0900

    Install axios and vue-axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router).use(VueAxios, axios);
 
-app.mount('#app')
+app.mount("#app");
