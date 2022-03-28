f9a4cf85d153e248f5153a4cb3836261479d858c
Sora Arakawa <mail@arkw.net>
2022-03-28 17:40:44 +0900

    Generate graph from API response data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,7 +27,28 @@ export default {
   },
   methods: {
     changePrefecture: function (selectedPrefecture) {
-      this.chartOptions.series[0].data = selectedPrefecture;
+      let vm = this;
+      let output = [];
+      this.axios
+        .get(
+          "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear",
+          {
+            headers: { "X-API-KEY": import.meta.env.VITE_APIKEY },
+            params: {
+              prefCode: selectedPrefecture[0],
+              cityCode: "-",
+            },
+          }
+        )
+        .then((response) => {
+          for (let i = 0; i < response.data.result.data[0].data.length; i++) {
+            output.push(response.data.result.data[0].data[i].value);
+          }
+          vm.chartOptions.series[0].data = output;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     },
   },
 };
