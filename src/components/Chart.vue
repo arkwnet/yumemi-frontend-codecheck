c2dc6bbedbd9a407ca9d3d72806f634a45f785a0
Sora Arakawa <mail@arkw.net>
2022-03-28 22:25:01 +0900

    Support multiple prefectures

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="chart">
-    <highcharts class="hc" :options="chartOptions"></highcharts>
+    <highcharts v-if="showChart" class="hc" :options="chartOptions"></highcharts>
   </div>
 </template>
 
 <script>
+import { nextTick } from "vue";
 export default {
   name: "Chart",
   components: {},
   props: ["options"],
   data() {
     return {
+      showChart: true,
       chartOptions: {},
     };
   },
   mounted: function () {
     this.chartOptions = this.options;
   },
+  methods: {
+    updateChart: function () {
+      nextTick(() => {
+        this.showChart = true;
+      })
+    },
+    hideChart: function () {
+      this.showChart = false;
+    }
+  }
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Prefecture @change-prefecture="changePrefecture" />
-    <Chart :options="chartOptions" />
+    <Chart ref="chart" :options="chartOptions" />
   </div>
 </template>
 
@@ -23,39 +23,47 @@ export default {
             pointInterval: 5,
           },
         },
-        series: [
-          {
-            data: [],
-          },
-        ],
+        series: [],
       },
     };
   },
   methods: {
     changePrefecture: function (selectedPrefecture) {
+      this.$refs.chart.hideChart()
       let vm = this;
       let output = [];
-      this.axios
-        .get(
-          "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear",
-          {
-            headers: { "X-API-KEY": import.meta.env.VITE_APIKEY },
-            params: {
-              prefCode: selectedPrefecture[0],
-              cityCode: "-",
-            },
-          }
-        )
-        .then((response) => {
-          for (let i = 0; i < response.data.result.data[0].data.length; i++) {
-            output.push(response.data.result.data[0].data[i].value);
-          }
-          vm.chartOptions.series[0].data = output;
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+      let temp = vm.ResetTemp("Figure");
+      for (let i = 0; i < selectedPrefecture.length; i++) {
+        this.axios
+          .get(
+            "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear",
+            {
+              headers: { "X-API-KEY": import.meta.env.VITE_APIKEY },
+              params: {
+                prefCode: selectedPrefecture[i],
+                cityCode: "-",
+              },
+            }
+          )
+          .then((response) => {
+            for (let j = 0; j < response.data.result.data[0].data.length; j++) {
+              temp.data.push(response.data.result.data[0].data[j].value);
+            }
+            output.push(temp);
+            temp = vm.ResetTemp("Figure");
+            if (i == selectedPrefecture.length - 1) {
+              vm.chartOptions.series = output;
+              vm.$refs.chart.updateChart();
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
     },
+    ResetTemp: function (prefectureName) {
+      return {name: prefectureName, data: []};
+    }
   },
 };
 </script>
